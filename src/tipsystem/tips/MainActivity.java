cc14a2cba9c995ccc5184c20fbb9ad37f2fb66ba
Sewon Na <rightsna@Sewon-ui-MacBook-Air.local>
2013-06-14 16:41:38 +0900

    no message

--- src/tipsystem/tips/MainActivity.java
+++ src/tipsystem/tips/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends Activity {
         
         textView = (TextView) findViewById(R.id.textViewShopIP);
         textView.setTypeface(typeface);
-        
     }
 
 
