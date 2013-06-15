27b4499e131ffedb2b51dd4af1ae995a9339b4ba
Sewon Na <rightsna@Sewon-ui-MacBook-Air.local>
2013-06-15 21:31:42 +0900

    -인증 추가 -AsyncTask 모듈화

--- src/tipsystem/tips/MainActivity.java
+++ src/tipsystem/tips/MainActivity.java
@@ -3,20 +3,24 @@ package tipsystem.tips;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import tipsystem.tips.ManageCustomerActivity.MyAsyncTask;
+import tipsystem.utils.ResultSetConverter;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.util.Log;
@@ -31,6 +35,9 @@ public class MainActivity extends Activity {
 	
 	public final static String EXTRA_MESSAGE = "unikys.todo.MESSAGE";
 
+	// loading bar
+	private ProgressDialog dialog; 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,59 +67,64 @@ public class MainActivity extends Activity {
     }
     
     public void onAuthentication(View view) {
-    	
+
+    	dialog = new ProgressDialog(this);
+ 		dialog.setMessage("Loading....");
+ 		dialog.setCancelable(false);
+ 		dialog.show();
+ 		
     	EditText textView = (EditText) findViewById(R.id.editTextShopCode);
     	String code = textView.getText().toString();
-    	if (!code.equals(""))
-    		new MyAsyncTask ().execute(code);
-    }    
-    
-    public void didLogin() {
+    	if (code.equals("")) return;
 
-        Toast.makeText(getApplicationContext(), "로그인 완료", 0).show();
-    	Intent intent = new Intent(this, ConfigActivity.class);
-    	startActivity(intent);
+	    String query =  "";
+	    //query = "select * from V_OFFICE_USER where Sto_CD =" + code + ";";
+	    query =  "select * " 
+	    		+ "from V_OFFICE_USER, APP_SETTLEMENT " 
+	    		+ "where Sto_CD = " + code
+	    		+ ";";
+		new MyAsyncTask ().execute("122.49.118.102:18971", "Trans", "app_tips", "app_tips", query);
+    }
+    
+    public void didLogin(JSONArray results) {
+    	if (results.length() > 0) {
+    		Toast.makeText(getApplicationContext(), "인증 완료", 0).show();
+        	Intent intent = new Intent(this, ConfigActivity.class);
+        	startActivity(intent);
+    	}
+    	else {
+    		Toast.makeText(getApplicationContext(), "인증 실패", 0).show();
+    	}
     }
     
 	// MSSQL
-    class MyAsyncTask extends AsyncTask<String, Integer, JSONObject> {
+    class MyAsyncTask extends AsyncTask<String, Integer, JSONArray> {
 
         ArrayList<JSONObject> CommArray=new ArrayList<JSONObject>();
         
-        protected JSONObject doInBackground(String... params) {
+        protected JSONArray doInBackground(String... params) {
         	Log.i("MSSQL"," MSSQL Connect Example.");
         	Connection conn = null;
-        	ResultSet reset =null;
-			JSONObject Obj = new JSONObject();
-        	String code = params[0];
+        	JSONArray json = null;
+        	String ip = params[0];
+        	String dbname = params[1];
+        	String dbid = params[2];
+        	String dbpw = params[3];
+        	String query = params[4];
+            Log.e("MSSQL","query: " + query );
         	
         	try {
         	    Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
         	    Log.i("MSSQL","MSSQL driver load");
 
-        	    conn = DriverManager.getConnection("jdbc:jtds:sqlserver://122.49.118.102:18971/Trans","app_tips","app_tips");
+        	    conn = DriverManager.getConnection("jdbc:jtds:sqlserver://" +ip +"/"+ dbname, dbid, dbpw);
         	    Log.i("MSSQL","MSSQL open");
         	    Statement stmt = conn.createStatement();
-
-        	    //String query =  "select * from APP_USER ";
-        	    String query =  "select * from APP_USER where OFFICE_CODE =" + code;
-	            Log.e("MSSQL","query: " + query );
-	
-	        	reset = stmt.executeQuery(query);
-
-        	    while(reset.next()){
-				    Obj.put("OFFICE_CODE",reset.getString(1).trim());
-				    Obj.put("APP_HP",reset.getString(2).trim());
-				    Obj.put("APP_USERNAME",reset.getString(3).trim());
-				    Obj.put("WRITE_DATE",reset.getString(4).trim());
-				    Obj.put("WRITER",reset.getString(5).trim());
-				    Obj.put("EDIT_DATE",reset.getString(6).trim());
-				    Obj.put("EDITOR",reset.getString(7).trim());
-				    Obj.put("BIGO",reset.getString(8).trim());
-				    
-					Log.w("MSSQL:", "data: " +Obj.toString());
-				}
         	    
+	        	ResultSet rs =null;
+	            rs = stmt.executeQuery(query);	            
+	        	json = ResultSetConverter.convert(rs);
+        	   
         	    conn.close();
         	
         	 } catch (Exception e) {
@@ -121,17 +133,21 @@ public class MainActivity extends Activity {
         	 
         	 // onProgressUpdate에서 0이라는 값을 받아서 처리
         	 publishProgress(0);
-        	 return Obj;        	 
+        	 return json;        	 
         }
 
         protected void onProgressUpdate(Integer[] values) {
             Log.e("MSSQL", "onProgressUpdate" );
         }
 
-        protected void onPostExecute(JSONObject result) {
-        	super.onPostExecute(result);
-			Log.w("MSSQL:", "result: " +result.toString());
-        	didLogin();
+        protected void onPostExecute(JSONArray results) {
+        	super.onPostExecute(results);
+			Log.w("MSSQL:", "onPostExecute: " +results.toString());
+
+			dialog.dismiss();
+			dialog.cancel();
+			
+        	didLogin(results);
         }
     };
 }
