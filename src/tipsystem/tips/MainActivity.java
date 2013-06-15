c81c384cf6e4fd4d28e2bec9b9500bf8318a9c55
Sewon Na <rightsna@Sewon-ui-MacBook-Air.local>
2013-06-15 21:59:46 +0900

    AsyncTask 모듈화

--- src/tipsystem/tips/MainActivity.java
+++ src/tipsystem/tips/MainActivity.java
@@ -1,33 +1,18 @@
 package tipsystem.tips;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import tipsystem.tips.ManageCustomerActivity.MyAsyncTask;
-import tipsystem.utils.ResultSetConverter;
+import tipsystem.utils.MSSQL;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -66,88 +51,49 @@ public class MainActivity extends Activity {
     	textView.setText("0000001");
     }
     
+    // 인증관련 실행 함수 
     public void onAuthentication(View view) {
 
+    	// 로딩 다이알로그 
     	dialog = new ProgressDialog(this);
  		dialog.setMessage("Loading....");
  		dialog.setCancelable(false);
  		dialog.show();
  		
+ 		// 입력된 코드 가져오기
     	EditText textView = (EditText) findViewById(R.id.editTextShopCode);
     	String code = textView.getText().toString();
     	if (code.equals("")) return;
 
+    	// 쿼리 작성하기
 	    String query =  "";
 	    //query = "select * from V_OFFICE_USER where Sto_CD =" + code + ";";
 	    query =  "select * " 
 	    		+ "from V_OFFICE_USER, APP_SETTLEMENT " 
 	    		+ "where Sto_CD = " + code
 	    		+ ";";
-		new MyAsyncTask ().execute("122.49.118.102:18971", "Trans", "app_tips", "app_tips", query);
+	    
+	    // 콜백함수와 함께 실행
+	    new MSSQL(new MSSQL.MSSQLCallbackInterface() {
+
+			@Override
+			public void onRequestCompleted(JSONArray results) {
+				dialog.dismiss();
+				dialog.cancel();
+				didAuthentication(results);
+			}
+	    }).execute("122.49.118.102:18971", "Trans", "app_tips", "app_tips", query);
     }
     
-    public void didLogin(JSONArray results) {
+    // DB에 접속후 호출되는 함수
+    public void didAuthentication(JSONArray results) {
     	if (results.length() > 0) {
-    		Toast.makeText(getApplicationContext(), "인증 완료", 0).show();
+    		Toast.makeText(getApplicationContext(), "인증 완료", Toast.LENGTH_SHORT).show();
         	Intent intent = new Intent(this, ConfigActivity.class);
         	startActivity(intent);
     	}
     	else {
-    		Toast.makeText(getApplicationContext(), "인증 실패", 0).show();
+    		Toast.makeText(getApplicationContext(), "인증 실패", Toast.LENGTH_SHORT).show();
     	}
     }
-    
-	// MSSQL
-    class MyAsyncTask extends AsyncTask<String, Integer, JSONArray> {
-
-        ArrayList<JSONObject> CommArray=new ArrayList<JSONObject>();
-        
-        protected JSONArray doInBackground(String... params) {
-        	Log.i("MSSQL"," MSSQL Connect Example.");
-        	Connection conn = null;
-        	JSONArray json = null;
-        	String ip = params[0];
-        	String dbname = params[1];
-        	String dbid = params[2];
-        	String dbpw = params[3];
-        	String query = params[4];
-            Log.e("MSSQL","query: " + query );
-        	
-        	try {
-        	    Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-        	    Log.i("MSSQL","MSSQL driver load");
-
-        	    conn = DriverManager.getConnection("jdbc:jtds:sqlserver://" +ip +"/"+ dbname, dbid, dbpw);
-        	    Log.i("MSSQL","MSSQL open");
-        	    Statement stmt = conn.createStatement();
-        	    
-	        	ResultSet rs =null;
-	            rs = stmt.executeQuery(query);	            
-	        	json = ResultSetConverter.convert(rs);
-        	   
-        	    conn.close();
-        	
-        	 } catch (Exception e) {
-        	    Log.w("Error connection","" + e.getMessage());		   
-        	 }
-        	 
-        	 // onProgressUpdate에서 0이라는 값을 받아서 처리
-        	 publishProgress(0);
-        	 return json;        	 
-        }
-
-        protected void onProgressUpdate(Integer[] values) {
-            Log.e("MSSQL", "onProgressUpdate" );
-        }
-
-        protected void onPostExecute(JSONArray results) {
-        	super.onPostExecute(results);
-			Log.w("MSSQL:", "onPostExecute: " +results.toString());
-
-			dialog.dismiss();
-			dialog.cancel();
-			
-        	didLogin(results);
-        }
-    };
 }
